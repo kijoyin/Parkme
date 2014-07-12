813f441fe642d3e30404316708a55d8fc9e74b43
kijoyin <kijoyin@gmail.com>
2014-07-13 00:05:57 +1000

    Front end changes

--- Parkme/Parkme/Controllers/HomeController.cs
+++ Parkme/Parkme/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,7 +13,16 @@ namespace Parkme.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            HttpWebRequest request = WebRequest.Create("https://maps.googleapis.com/maps/api/geocode/json?address=" + "Briabane") as HttpWebRequest;
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                // Get the response stream  
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                var results = JsonConvert.DeserializeObject<dynamic>(reader.ReadToEnd());
+                var Lat = results.results[0].geometry.location.lat;
+                var Long = results.results[0].geometry.location.lng;
+                return View();
+            }
         }
 
         public ActionResult About()
--- Parkme/Parkme/Scripts/app/app.js
+++ Parkme/Parkme/Scripts/app/app.js
@@ -1,49 +1,42 @@
-﻿//function initialize() {
+﻿////function initialize() {
+////    var service = new google.maps.places.AutocompleteService();
+////    service.getQueryPredictions({ input: 'brisbane' }, callback);
+////}
+
+////function callback(predictions, status) {
+////    if (status != google.maps.places.PlacesServiceStatus.OK) {
+////        alert(status);
+////        return;
+////    }
+
+////    var results = document.getElementById('results');
+
+////    for (var i = 0, prediction; prediction = predictions[i]; i++) {
+////        results.innerHTML += '<li>' + prediction.description + '</li>';
+////    }
+////}
+
+////google.maps.event.addDomListener(window, 'load', initialize);
+///**
+// * Author: Richard Willis - badsyntax.co
+// * Example here:  http://demos.badsyntax.co/places-search-bootstrap/example.html
+// *
+// * Please note: This is not a reliable method of geocoding the address. Using the 
+// * PlacesService is a much better approach. View the example above for an example 
+// * of using the PlacesService to geocode the address.
+// */
+//$(document).ready(function () {
 //    var service = new google.maps.places.AutocompleteService();
-//    service.getQueryPredictions({ input: 'brisbane' }, callback);
-//}
+//    var geocoder = new google.maps.Geocoder();
 
-//function callback(predictions, status) {
-//    if (status != google.maps.places.PlacesServiceStatus.OK) {
-//        alert(status);
-//        return;
+//    $('#typeahead').typeahead({
+//        hint: true,
+//        highlight: true,
+//        minLength: 1
+//    },  
+//    {
+//        source: 
+//        }
 //    }
-
-//    var results = document.getElementById('results');
-
-//    for (var i = 0, prediction; prediction = predictions[i]; i++) {
-//        results.innerHTML += '<li>' + prediction.description + '</li>';
-//    }
-//}
-
-//google.maps.event.addDomListener(window, 'load', initialize);
-/**
- * Author: Richard Willis - badsyntax.co
- * Example here:  http://demos.badsyntax.co/places-search-bootstrap/example.html
- *
- * Please note: This is not a reliable method of geocoding the address. Using the 
- * PlacesService is a much better approach. View the example above for an example 
- * of using the PlacesService to geocode the address.
- */
-$(document).ready(function () {
-    var service = new google.maps.places.AutocompleteService();
-    var geocoder = new google.maps.Geocoder();
-
-    $('#typeahead').typeahead({
-        hint: true,
-        highlight: true,
-        minLength: 1
-    },  
-    {
-        source: function (query, process) {
-            service.getPlacePredictions({ input: query }, function (predictions, status) {
-                if (status == google.maps.places.PlacesServiceStatus.OK) {
-                    process($.map(predictions, function (prediction) {
-                        return prediction.description;
-                    }));
-                }
-            });
-        }
-    }
-    });
-});
+//    });
+//});
