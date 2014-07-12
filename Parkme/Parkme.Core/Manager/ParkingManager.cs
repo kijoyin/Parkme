9be452857f1d3d949102956199fba047ad264946
Dominic Joseph <dojovalumannel@gmail.com>
2014-07-12 17:45:45 +1000

    parking manager updated

--- Parkme/Parkme.Core/Manager/ParkingManager.cs
+++ Parkme/Parkme.Core/Manager/ParkingManager.cs
@@ -17,9 +17,24 @@ namespace Parkme.Core.Manager
             while (!reader.EndOfStream)
             {
                 var line = reader.ReadLine();
-                var values = line.Split(';');
+                var values = line.Split(',');
                 Parking parkingTmpo = new Parking();
                 parkingTmpo.MeterNo = values[0];
+                parkingTmpo.Category = values[1];
+                parkingTmpo.Street = values[2];
+                parkingTmpo.Suburb = values[3];
+                parkingTmpo.MaxStay = values[4];
+                parkingTmpo.Restrictions = values[5];
+                parkingTmpo.OperationalDay = values[6];
+                parkingTmpo.OperationsTime = values[7];
+                parkingTmpo.RateWeekDay = values[9];
+                parkingTmpo.RateWeekEnd = values[10];
+                parkingTmpo.LocationDescription = values[11];
+                parkingTmpo.VehicleBay = values[12];
+                parkingTmpo.MotorCycleBay = values[13];
+                parkingTmpo.MotorCycleRate = values[14];
+                parkingTmpo.Lat = Convert.ToDecimal(values[15]);
+                parkingTmpo.Long = Convert.ToDecimal(values[16]);
                 parkings.Add(parkingTmpo);
             }
             return parkings;
