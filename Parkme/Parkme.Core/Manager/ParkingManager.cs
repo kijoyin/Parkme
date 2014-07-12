4a642c529185a9ae896745b0f7cdfcbf97a2d0a2
dojovalumannel <dojovalumannel@gmail.com>
2014-07-12 21:28:30 +1000

    Revert "deimal"
    This reverts commit 511049af90efba2291b676e1af51b45133b04a11.

--- Parkme/Parkme.Core/Manager/ParkingManager.cs
+++ Parkme/Parkme.Core/Manager/ParkingManager.cs
@@ -27,16 +27,22 @@ namespace Parkme.Core.Manager
                 parkingTmpo.Restrictions = values[5];
                 parkingTmpo.OperationalDay = values[6];
                 parkingTmpo.OperationsTime = values[7];
-                if (Decimal.TryParse(values[9], out rateWeekDay))
-                { 
-                parkingTmpo.RateWeekDay =rateWeekDay
-                }
+                parkingTmpo.RateWeekDay = values[9];
                 parkingTmpo.RateWeekEnd = values[10];
                 parkingTmpo.LocationDescription = values[11];
                 parkingTmpo.VehicleBay = values[12];
                 parkingTmpo.MotorCycleBay = values[13];
                 parkingTmpo.MotorCycleRate = values[14];
-                parkingTmpo.Lat = Convert.ToDecimal(values[15]);
+                decimal lat = new decimal();
+                decimal lon = new decimal();
+                if (Decimal.TryParse(values[15], out lat))
+                {
+                    parkingTmpo.Lat = lat;
+                }
+                if (Decimal.TryParse(values[16], out lon))
+                {
+                    parkingTmpo.Long = lon;
+                }
                 parkingTmpo.Long = Convert.ToDecimal(values[16]);
                 parkings.Add(parkingTmpo);
             }
